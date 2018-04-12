53160666f4055ab14ca4baf482e418c8c4039e57
sharmilashahul <sharmi6197@gmail.com>
2018-04-12 07:26:10 +0000

    Configured Spring Motor Control partially for server input

--- motor_control_spring.py
+++ motor_control_spring.py
@@ -2,16 +2,16 @@ import RPi.GPIO as GPIO
 from time import sleep
 import time
 
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3, GPIO.IN)
-i=GPIO.input(3)
+i = GPIO.input(3)
 print(i)
 
 motora = 7
-motorb = 8 # Input Pin
+motorb = 8  # Input Pin
 motorc = 5  # Enable Pin
+
 
 def set():
     GPIO.setmode(GPIO.BOARD)
@@ -20,35 +20,38 @@ def set():
     GPIO.setup(motorc, GPIO.OUT)
     GPIO.setwarnings(False)
 
+
 def backward():
     set()
-    print"BACKWARD MOTION"
     GPIO.output(motora, GPIO.HIGH)
     GPIO.output(motorb, GPIO.LOW)
     GPIO.output(motorc, GPIO.HIGH)
 
+
 def forward():
     set()
-    print"FORWARD MOTION"
     GPIO.output(motora, GPIO.LOW)
     GPIO.output(motorb, GPIO.HIGH)
     GPIO.output(motorc, GPIO.HIGH)
 
+
 def stop():
     set()
-    print"STOP"
     GPIO.output(motorc, GPIO.LOW)
 
+flag = True
 while True:
-    i = GPIO.input(3)
-    if i==1:
+    if flag:
+        print "Waiting for Server Confirmation"
+        a = raw_input()
+        flag = False
         forward()
-        print "rotating",i
         time.sleep(0.5)
-    if i==0:
+    i = GPIO.input(3)
+    if i == 0:
+        flag = True
         stop()
-        print "stop",i
         time.sleep(0.5)
     else:
         forward()
-        print "wait for input"
+        time.sleep(0.5)
